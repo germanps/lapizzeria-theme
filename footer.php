6b79af16c9e8cdd7387110e2b27bb93125340285
Germán Pla <germanps@outlook.es>
2017-08-30 13:52:43 +0200

    Imprimir Menú FOOTER y pié de página

--- footer.php
+++ footer.php
@@ -1,3 +1,31 @@
+	
+
+
+
+
+
+
+	<footer>
+		<?php 
+			$args = array(
+				'theme-location' => 'header-menu',
+				'container' => 'nav',
+				'after' => '<span class="separador"> | </span>'
+			);
+			wp_nav_menu( $args );
+		?>
+		<div class="ubicacion">
+			<p>Bay Avenue Mountain View, CA 94043</p>
+			<p>Teléfono: +1-92-456-7890</p>
+		</div>
+	</footer>
+
+
+
+
+
+
+
 	<?php wp_footer(); ?>
 	</body>
 </html>
