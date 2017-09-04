5da0acafedb26f322a2fe44b5986bb9405c2a1f2
Germán Pla <germanps@outlook.es>
2017-09-04 17:27:09 +0200

    Custom Post Types para Pizzas y especialidades en la barra lateral del backend de wordpress

--- functions.php
+++ functions.php
@@ -52,5 +52,49 @@ add_action('init', 'lapizzeria_menus');
 
 
 
+/******************************************************************************/
+/******************************* Custom PostTypes *****************************/
+/******************************************************************************/
+add_action( 'init', 'lapizzeria_especialidades' );
+function lapizzeria_especialidades() {
+	$labels = array(
+		'name'               => _x( 'Pizzas', 'lapizzeria' ),
+		'singular_name'      => _x( 'Pizzas', 'post type singular name', 'lapizzeria' ),
+		'menu_name'          => _x( 'Pizzas', 'admin menu', 'lapizzeria' ),
+		'name_admin_bar'     => _x( 'Pizzas', 'add new on admin bar', 'lapizzeria' ),
+		'add_new'            => _x( 'Añadir Nuevo', 'book', 'lapizzeria' ),
+		'add_new_item'       => __( 'Añadir Nueva Pizza', 'lapizzeria' ),
+		'new_item'           => __( 'Nuevo Pizzas', 'lapizzeria' ),
+		'edit_item'          => __( 'Editar Pizzas', 'lapizzeria' ),
+		'view_item'          => __( 'Ver Pizzas', 'lapizzeria' ),
+		'all_items'          => __( 'Todas Pizzas', 'lapizzeria' ),
+		'search_items'       => __( 'Search Pizzas', 'lapizzeria' ),
+		'parent_item_colon'  => __( 'Parent Pizzas:', 'lapizzeria' ),
+		'not_found'          => __( 'No Pizzases found.', 'lapizzeria' ),
+		'not_found_in_trash' => __( 'No Pizzases found in Trash.', 'lapizzeria' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+    	'description'        => __( 'Description.', 'lapizzeria' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'especialidades' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 6,
+		'supports'           => array( 'title', 'editor', 'thumbnail' ),
+    	'taxonomies'         => array( 'category' ),
+	);
+
+	register_post_type( 'especialidades', $args );
+}
+/******************************************************************************/
+/***************************** Fin Custom PostTypes ***************************/
+/******************************************************************************/
 
  ?>
