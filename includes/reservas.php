8656f0dbf6be1f87d6d6516abe8c4c7e807413bc
germanps-home <germanps@outlok.es>
2017-09-18 11:26:43 +0200

    Sanitización de variables del formulario (seguridad)

--- includes/reservas.php
+++ includes/reservas.php
@@ -3,6 +3,11 @@
 function lapizzeria_guardar_datos() {
 	if (isset($_POST['enviar']) && $_POST['oculto'] == "1") :
 		
+		$nombre = sanitize_text_field( $_POST['nombre'] );
+		$fecha = sanitize_text_field( $_POST['fecha'] );
+		$email = sanitize_text_field( $_POST['email'] );
+		$telefono = sanitize_text_field( $_POST['telefono'] );
+		$mensaje = sanitize_text_field( $_POST['mensaje'] );
 
 		
 	endif;
