ee43d4f4d17cc260b21c6ee94b36ca4e4e1f35b9
germanps-home <germanps@outlok.es>
2017-09-19 15:14:37 +0200

    Creación de Options para el Theme II

--- includes/options.php
+++ includes/options.php
@@ -25,9 +25,54 @@ function lapizzeria_ajustes(){
 
 add_action( 'admin_menu', 'lapizzeria_ajustes' );
 
+
 function lapizzeria_opciones() {
 
 }
 
+function lapizzeria_reservas() {
+
+	?>
+		
+	<div class="wrap">
+		<h1>Reservas</h1>
+		<table class="wp-list-table widefat striped">
+			<thead>
+				<tr>
+					<th class="manage-column">ID</th>
+					<th class="manage-column">Nombre</th>
+					<th class="manage-column">Fecha de Reserva</th>
+					<th class="manage-column">Email</th>
+					<th class="manage-column">Teléfono</th>
+					<th class="manage-column">Mensaje</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+					global $wpdb;
+					$reservas = $wpdb->prefix . 'reservas';
+					$registros = $wpdb->get_results( "SELECT * FROM $reservas;", ARRAY_A);
+					foreach ($registros as $registro) { ?>
+						<tr>
+							<td><?php echo $registro['id'] ?></td>
+							<td><?php echo $registro['nombre'] ?></td>
+							<td><?php echo $registro['fecha'] ?></td>
+							<td><?php echo $registro['email'] ?></td>
+							<td><?php echo $registro['telefono'] ?></td>
+							<td><?php echo $registro['mensaje'] ?></td>
+						</tr>
+
+						
+					<?php } ?>
+
+				
+
+			</tbody>
+		</table>
+	</div>
+
+	<?php  
+
+}
 
 ?>
